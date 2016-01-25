73e11ca199507e805718d3b5be0568db6dd546bc
Toks <mthiamang@gmail.com>
2016-01-25 15:23:27 -0500

    More solutions

---
+++ statistics/applied_multivariate_statistical_analysis_problems/chapter_1/E1.14_TH.R
@@ -0,0 +1,15 @@
+# Import dat file. 
+t="/Users/thiamang/Documents/github/personal/tmh-scripts/statistics/applied_multivariate_statistical_analysis_problems/wichern_data/T1-6.dat"
+raw_data = read.table(t,header=FALSE, skip=0)
+head(raw_data)
+colnames(raw_data)=c("Age", "S1LplusS1R", "S1LminusS1R", "S2LplusS2R", "S2LminusS2R")
+head(raw_data)
+attach(raw_data)
+subset=raw_data[c('S1LplusS1R', 'S2LplusS2R')]
+#Now create scatter plot.
+plot(S1LplusS1R, S2LplusS2R)
+#Now find the summary sta
+lapply(subset, mean)
+var(subset)
+cov(subset)
+cor(subset)
